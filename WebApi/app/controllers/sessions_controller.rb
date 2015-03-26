c27af85ac2bb67abe075a5b41e5ebc58e9299f51
ba222ec <ba222ec@student.lnu.se>
2015-03-26 22:32:26 +0000

    Ändrar responsen vid autenticieringsförsök.

--- WebApi/app/controllers/sessions_controller.rb
+++ WebApi/app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if creator && creator.authenticate(params[:password])
       # auth_token valid one hour 
       auth_token = AuthToken.new(creator.id, Time.now + 1.hours)
-      render json: { auth_token: encodeJWT(auth_token), terminates_at: (Time.now.utc + 1.hours).to_r }
+      render json: { auth_token: encodeJWT(auth_token), terminates_at: Time.now.utc + 1.hours }
     else
       message = ErrorMessage.new(
         "Unvalid email or password", 
